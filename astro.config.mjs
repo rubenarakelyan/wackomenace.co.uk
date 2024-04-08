5cc7033002e9f62992d9e49bfe2fa670213193d9
Ruben Arakelyan <ruben@arakelyan.uk>
2024-04-08 15:44:04 +0100

    Add guestbook

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
+import db from "@astrojs/db";
 import mdx from "@astrojs/mdx";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
@@ -9,22 +10,14 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: "compile"
+  }),
   site: process.env.NODE_ENV === "development" ? "http://localhost:4321" : "https://www.wackomenace.co.uk",
-  integrations: [
-    mdx({
-      syntaxHighlight: false,
-      rehypePlugins: [
-        rehypeSlug,
-        [
-          rehypePrettyCode,
-          {
-            theme: "github-dark",
-          },
-        ],
-      ],
-    }),
-    sitemap(),
-    tailwind(),
-  ],
+  integrations: [db(), mdx({
+    syntaxHighlight: false,
+    rehypePlugins: [rehypeSlug, [rehypePrettyCode, {
+      theme: "github-dark"
+    }]]
+  }), sitemap(), tailwind()]
 });
