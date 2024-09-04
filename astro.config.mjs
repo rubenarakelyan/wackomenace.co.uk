fd145c132229c70ce9871215eb7de2d561e181f1
Ruben Arakelyan <ruben@arakelyan.uk>
2024-09-04 16:33:04 +0100

    Add redirects from removed pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,9 @@ export default defineConfig({
     "/.well-known/recommendations.json": "/blogroll/rubenarakelyan.json",
     "/.well-known/recommendations.opml": "/blogroll/rubenarakelyan.opml",
     "/.well-known/webfinger": "/.well-known/webfinger.json",
-    "/blog": "/"
+    "/blog": "/",
+    "/blog/random": "/blog/16-dynamic-to-static/#no-more-random-blog-posts",
+    "/guestbook": "/blog/16-dynamic-to-static/#guestbook---gone"
   },
   vite: {
     ssr: {
